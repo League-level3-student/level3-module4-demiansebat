edaf00c238280b5219ddca58906f268c28d70a6b
league <league@imac-18.attlocal.net>
2023-11-04 16:31:21 -0700

    need to do next steps text undo redo

--- src/_00_IntroToStacks/_02_TextUndoRedo.java
+++ src/_00_IntroToStacks/_02_TextUndoRedo.java
@@ -1,19 +1,27 @@
 package _00_IntroToStacks;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class _02_TextUndoRedo {
-	
+public class _02_TextUndoRedo implements KeyListener {
+	public static void main(String[] args) {
+		_02_TextUndoRedo bruh= new _02_TextUndoRedo();
+		bruh.setup();
+	}
 JFrame frame= new JFrame();
 JPanel panel=new JPanel();
 JLabel label= new JLabel();
 void setup() {
 	frame.add(panel);
 	panel.add(label);
-}
-    /* 
+	frame.addKeyListener(this);    
+	frame.setVisible(true);
+	frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+	frame.pack();/* 
      * Create a JFrame with a JPanel and a JLabel.
      * 
      * Every time a key is pressed, add that character to the JLabel. It should
@@ -30,4 +38,21 @@ void setup() {
      */
 
 
+}
+@Override
+public void keyTyped(KeyEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+@Override
+public void keyPressed(KeyEvent e) {
+	// TODO Auto-generated method stub
+	char pressed=e.getKeyChar();
+	label.setText(label.getText()+pressed);
+}
+@Override
+public void keyReleased(KeyEvent e) {
+	// TODO Auto-generated method stub
+	
+}
 }
