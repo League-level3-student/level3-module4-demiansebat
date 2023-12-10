91c12595368a9a600549a95c157d778c50b5cebf
league <league@imac-18.attlocal.net>
2023-12-09 16:37:03 -0800

    baseball ip need to make boolean for while

--- src/_02_IntroToQueues/_02_BaseballTickets.java
+++ src/_02_IntroToQueues/_02_BaseballTickets.java
@@ -26,16 +26,60 @@
 package _02_IntroToQueues;
 
 import java.util.ArrayDeque;
+import java.util.Stack;
 
 /*
- * Complete the calculateWaitTime() method here!
- * Instructions are in the BaseBallTicketsTest class.
+ * OBJECTIVE:
+ * Complete the calculateWaitTime() method to return the amount of time it
+ * takes to get the tickets.
+ *  
+ * You managed to send your friend to queue for tickets instead of doing it yourself because your lazy, but
+ * there is a catch: he will get there only if you tell him how much that is
+ * going to take. And everybody can only take ONE TICKET at a time, then they
+ * go back in the last position of the queue if they need more (or go home if
+ * they are fine).
+ * 
+ * Each ticket takes one minutes to emit with no wasted time between purchases.
+ * You will be given a queue with all the people queuing and the initial
+ * position of your buddy.
+ * 
+ * Example:
+ * queue: [2, 5, 3, 4, 6]
+ * position: 1 (the person who wants to buy 5 tickets)
+ * 
+ * The first person buys 1 ticket then moves to the back of the queue because
+ * they still need to buy 1 more ticket. Everyone else moves up the queue.
+ * queue: [5, 3, 4, 6, 1]
+ * 
+ * Now it's your turn to buy a ticket. You buy 1 and move to the back of the
+ * queue because you still need to buy 4 more
+ * queue: [3, 4, 6, 1, 4]
+ *
+ * For this queue it takes a total of 18 minutes!
  */
 
 public class _02_BaseballTickets {
 
     public static int calculateWaitTime( ArrayDeque<Integer> ticketsQueue, int position ) {
+    	int minutes=0;
+    	Stack <Integer> lastFirst= new Stack();
+  // make a boolean and do something while the boolean is true that position==0 and the thing at position 0 has a value of 0. they both have to be true.  while(/) {
+			int last=ticketsQueue.remove();
+			ticketsQueue.add(last-1);
+			minutes++;
+			if (last==0 && position==0) {
+				return minutes;
+			}
+		}
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+		return 0;
         
-        return -1;
+      
     }
 }
