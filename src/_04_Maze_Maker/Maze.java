a77bcb429753483c38419a8ddb45af972ac10a22
Hailin Day <haililnday@gmail.com>
2024-03-02 16:31:31 -0800

    working on MazeMaker

--- src/_04_Maze_Maker/Maze.java
+++ src/_04_Maze_Maker/Maze.java
@@ -1,38 +1,47 @@
 package _04_Maze_Maker;
+
 import java.awt.Graphics;
 
 public class Maze {
-    // 1. Create a 2D array of cells. Don't initialize it.
-    
-    private int rows;
-    private int cols;
+	// 1. Create a 2D array of cells. Don't initialize it.
+	Cell[][] cells;
+	private int rows;
+	private int cols;
 
-    public Maze(int rows, int cols) {
-        this.rows = rows;
-        this.cols = cols;
+	public Maze(int rows, int cols) {
+		this.rows = rows;
+		this.cols = cols;
 
-        // 2. Initialize the cells array using the rows and cols variables
+		// 2. Initialize the cells array using the rows and cols variables
+		cells = new Cell[rows][cols];
+		// 3. Iterate through each cell and initialize it
+		// using row and col as the cell location
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < cols; j++) {
+cells[i][j]= new Cell(i,j);
+			}
+		}
+	}
 
-        // 3. Iterate through each cell and initialize it
-        //    using row and col as the cell location
+	// 4. This method iterates through the cells and draws them
+	public void draw(Graphics g) {
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < cols; j++) {
+				cells[i][j].draw(g);
+			}
+		}
+	}
 
-    }
+	// 5. This method returns the selected cell
+	public Cell getCell(int row, int col) {
+		return cells[row][col];
+			}
 
-    // 4. This method iterates through the cells and draws them
-    public void draw(Graphics g) {
+	public int getRows() {
+		return rows;
+	}
 
-    }
-
-    // 5. This method returns the selected cell
-    public Cell getCell(int row, int col){
-        return  null;
-    }
-
-    public int getRows() {
-        return rows;
-    }
-
-    public int getCols() {
-        return cols;
-    }
+	public int getCols() {
+		return cols;
+	}
 }
